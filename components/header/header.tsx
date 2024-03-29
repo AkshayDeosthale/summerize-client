76925206b40aa5fbc9da105568ce4d8e6230c6dd
AkshayDeosthale <akshay.deosthale6@gmail.com>
2024-03-29 19:58:40 +0530

    [ADD] Logos

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,4 +1,5 @@
 import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
@@ -6,7 +7,19 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <header className="flex w-full justify-between items-center ">
-      <p>Summarize</p>
+      <div className="flex gap-4 items-center">
+        <Image
+          src="/logo.jpeg"
+          className="rounded-lg"
+          height={50}
+          width={50}
+          alt="logo"
+        />
+        <h1 className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-600">
+          SummaURL
+        </h1>
+      </div>
+
       <UserButton />
     </header>
   );
